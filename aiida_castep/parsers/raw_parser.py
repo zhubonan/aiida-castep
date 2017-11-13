3405f5a8b5a5b4ea0b7cb17a8d85531f22e94c9a
Bonan Zhu <bz240@cam.ac.uk>
2017-11-13 18:17:36 +0000

    Update raw_parser.py

--- aiida_castep/parsers/raw_parser.py
+++ aiida_castep/parsers/raw_parser.py
@@ -5,13 +5,50 @@ Module for parsing .castep file
 
 import os
 import re
+import numpy as np
 from aiida_castep.parsers import CASTEPOutputParsingError
 
 
-def parse_raw_ouput(outfile, input_dict, parser_opts=None):
+# TODO update CODATA for castep 16.1 and above
+# CODATA1986 (included herein for the sake of completeness)
+# taken from
+#    http://physics.nist.gov/cuu/Archive/1986RMP.pdf
+units_CODATA1986 = {
+    'hbar': 6.5821220E-16,      # eVs
+    'Eh': 27.2113961,           # eV
+    'kB': 8.617385E-5,          # eV/K
+    'a0': 0.529177249,          # A
+    'c': 299792458,             # m/s
+    'e': 1.60217733E-19,        # C
+    'me': 5.485799110E-4}       # u
+
+# CODATA2002: default in CASTEP 5.01
+# (-> check in more recent CASTEP in case of numerical discrepancies?!)
+# taken from
+#    http://physics.nist.gov/cuu/Document/all_2002.pdf
+
+units_CODATA2002 = {
+    'hbar': 6.58211915E-16,     # eVs
+    'Eh': 27.2113845,           # eV
+    'kB': 8.617343E-5,          # eV/K
+    'a0': 0.5291772108,         # A
+    'c': 299792458,             # m/s
+    'e': 1.60217653E-19,        # C
+    'me': 5.4857990945E-4}      # u
+
+# (common) derived entries
+for d in (units_CODATA1986, units_CODATA2002):
+    d['t0'] = d['hbar'] / d['Eh']     # s
+    d['Pascal'] = d['e'] * 1E30       # Pa
+units = units_CODATA2002
+
+unit_suffix = "_units"
+
+def parse_raw_ouput(outfile, input_dict, parser_opts=None, geom_file=None):
     """
     Parser an dot_castep file
     :param outfile: path to .castep file
+    :param geom_file: path to the .geom file
 
     :returns out_dict: a dictionary with parsed data
     :return trajectory_data: dictionary of trajectory data
@@ -44,8 +81,12 @@ def parse_raw_ouput(outfile, input_dict, parser_opts=None):
 
     finished_run = False
 
-    # T
-    for line in reversed(out_lines):
+    # Use Total time as a mark for completed run
+    for i, line in enumerate(reversed(out_lines)):
+        # Check only the last 20 lines
+        # Otherwise unfinished restarts may be seen as finished
+        if i >= 20:
+            break
         if "Total time" in line:
             finished_run = True
             break
@@ -59,6 +100,10 @@ def parse_raw_ouput(outfile, input_dict, parser_opts=None):
     # Parse the data and store
     try:
         out_data, trajectory_data, critical_messages = parse_castep_text_output(out_lines, input_dict)
+        if geom_file is not None:
+            with open(geom_file) as gfile:
+                glines = gfile.readlines()
+            geom_data = parser_geom_text_output(glines, None)
 
     except CASTEPOutputParsingError as e:
         if not finished_run:
@@ -91,10 +136,19 @@ def parse_castep_text_output(out_lines, input_dict):
     If any is found in parsed_data["warnings"] the calucaltion is failed.
     """
 
+    psedu_files = {}
     parsed_data = {}
     parsed_data["warnings"] = []
-    trajectory_data = {}
-    trajectory_data["enthalpy"] = []
+
+    # Initialise storage space for trajectory
+    # Not all is needed. But we parse as much as we can here
+    trajectory_data = {"total_energy":[],
+                       "free_energy":[],
+                       "zero_K_energy":[],
+                       "forces": [],
+                       "stress_tensor":[],
+                       "enthalpy":[],
+                       }
 
     critical_warnings = {"Geometry optimization failed to converge":
     "Maximum geometry optimization cycle has been reached",
@@ -103,21 +157,58 @@ def parse_castep_text_output(out_lines, input_dict):
     minor_warnings = {"Warning": None}
     all_warnings = dict(critical_warnings.items() + minor_warnings.items())
 
+    def pair_split(lines, spliter):
+        """Split the line in to two. Ignore space around spliter"""
+        lines.strip().split(spliter)
+
+    # Split header and body
+    for i, line in enumerate(out_lines):
+        if "Files used for pseudopotentials" in line:
+            for line in out_lines[i+1:]:
+                if "---" in line:
+                    break
+                else:
+                    try:
+                        specie, psedu_file = line.strip().split()
+                    except ValueError:
+                        break
+                    else:
+                        psedu_files.update({specie: psedu_file})
+
+        if "Point group of crystal" in line:
+            parsed_data["point_group"] = line.strip().split("=")[1].strip()
+
+        if "Space group of crystal" in line:
+            parsed_data["space_group"] = line.strip().split("=")[1].strip()
+
+        if "Cell constraints" in line:
+            parsed_data["cell_constraints"] = line.strip().split(":")[1].strip()
+        if "Number of kpoints used" in line:
+            parsed_data["n_kpoints"] = line.strip().split("=")[1].strip()
+
+        if "MEMORY AND SCRATCH DISK ESTIMATES" in line:
+            body_start = i
+
     # Parse non-repeating informaton e.g initialisation etc
-    for count, line in enumerate(out_lines):
+    for count, line in enumerate(out_lines[body_start:]):
         if "Calculation parallelised over" in line:
             num_cores = int(line.strip().split()[-2])
             parsed_data["parallel_procs"] = num_cores
 
         # For dm and fixed occupancy runs
-        if "Final free" in line or "Final energy" in line:
-            free_eng = line.strip().split()[-2]
-            parsed_data["energy"] = float(free_eng)
+        if "Final energy" in line:
+            trajectory_data["total_energy"].append(line.strip().split()[-2])
 
+        if "Final free" in line:
+            trajectory_data["free_energy"].append(line.strip().split()[-2])
+
+        if "0K energy" in line:
+            trajectory_data["zero_K_energy"].append(line.strip().split()[-2])
 
         if "finished iteration" in line:
             geom_H  = float(line.strip().split()[-2])
             trajectory_data["enthalpy"].append(geom_H)
+
         if any(i in line for i in all_warnings):
             message = [ all_warnings[i] for i in all_warnings.keys() if i in line][0]
             if message is None:
@@ -125,9 +216,70 @@ def parse_castep_text_output(out_lines, input_dict):
 
             parsed_data["warnings"].append(message)
 
-    return parsed_data, trajectory_data, critical_warnings.values()
+    return parsed_data, critical_warnings.values()
 
 
+# This function is modified from ase's geom reader
+def parser_geom_text_output(out_lines, input_dict):
+    """
+    Parse output of .geom file
+
+    :param out_lines: a list of lines from the readline function
+    :param input_dict: not in use at the moment
+
+    :return parsed_data: key, value of the trajectories of cell, atoms,
+    force etc
+    """
+    txt = out_lines
+    Hartree = units['Eh']
+    Bohr = units['a0']
+
+    # Yeah, we know that...
+    # print('N.B.: Energy in .geom file is not 0K extrapolated.')
+    cell_list = []
+    species_list = []
+    geom_list  = []
+    forces_list = []
+    energy_list = []
+
+    # Taken from ASE with minor modifications
+    for i, line in enumerate(txt):
+        if '<-- E' in line:
+            start_found = True
+            energy = float(line.split()[0]) * Hartree
+            cell = [x.split()[0:3] for x in txt[i + 1:i + 4]]
+            cell = np.array([[float(col) * Bohr for col in row] for row in
+                             cell])
+            cell_list.append(cell)
+            energy_list.append(energy)
+        if '<-- R' in line and start_found:
+            start_found = False
+            geom_start = i
+            for i, line in enumerate(txt[geom_start:]):
+                if '<-- F' in line > 0:
+                    geom_stop = i + geom_start
+                    break
+            species = [line.split()[0] for line in
+                       txt[geom_start:geom_stop]]
+            geom = np.array([[float(col) * Bohr for col in
+                              line.split()[2:5]] for line in
+                             txt[geom_start:geom_stop]])
+            forces = np.array([[float(col) * Hartree / Bohr for col in
+                                line.split()[2:5]] for line in
+                               txt[geom_stop:geom_stop +
+                                   (geom_stop - geom_start)]])
+            species_list.append(species)
+            geom_list.append(geom)
+            forces_list.append(forces)
+    if len(species_list) == 0:
+        raise CASTEPOutputParsingError("No data found in geom file")
+
+    return dict(cells = np.array(cell_list),
+                positons = np.array(geom_list),
+                forces = np.array(forces_list),
+                energies = np.array(energy_list),
+                symbols = species_list[0]
+                )
 
 
 
