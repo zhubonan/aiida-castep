e4b1fcb61095d1a690fe88485bd7583a302d38db
Bonan Zhu <bz240@cam.ac.uk>
2018-10-04 17:20:19 +0100

    Modified setep.py to allow loading of json datafile

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
+from os.path import abspath, dirname, join
 from setuptools import setup, find_packages
 import json
 
 if __name__ == '__main__':
     # Provide static information in setup.json
     # such that it can be discovered automatically
-    with open('setup.json', 'r') as info:
+    ROOT = abspath(dirname(__file__))
+    with open(join(ROOT, 'setup.json'), 'r') as info:
         kwargs = json.load(info)
     setup(
         packages=find_packages(),
