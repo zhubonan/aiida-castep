b9de675e80fc9aed202554c674fd1f609fbae457
Bonan Zhu <zhubonan@outlook.comBonan Zhu>
2019-03-27 08:30:27 +0000

    Clean up the new fixture mananger

--- aiida_castep/tests/dbtests/fixture.py
+++ aiida_castep/tests/dbtests/fixture.py
@@ -3,13 +3,12 @@ Fixture for reusing existing test db
 """
 
 from __future__ import absolute_import
-from aiida.backends.testbase import AiidaTestCase
+import os
+
+from aiida.backends.testbase import check_if_tests_can_run
 from aiida.manage.fixtures import FixtureManager
 from contextlib import contextmanager
-from aiida.backends.testbase import check_if_tests_can_run
-import pytest
 
-import os
 
 class ExistingProfileFixtureManager(FixtureManager):
     """
@@ -18,16 +17,6 @@ class ExistingProfileFixtureManager(FixtureManager):
 
     def __init__(self):
         super(ExistingProfileFixtureManager, self).__init__()
-        self.profile_info = {
-            'backend': 'django',
-            'email': 'test@aiida.mail',
-            'first_name': 'AiiDA',
-            'last_name': 'Plugintest',
-            'institution': 'aiidateam',
-            'db_user': 'aiida',
-            "db_pass": "lilURNxqbaJffkcXro1GqwnzBX0wnhM6LpwQd2BEfNesf4i8jW",
-            'db_name': 'test_aiida1.0'
-        }
         self._is_running_on_test_profile = False
 
     def create_profile(self):
@@ -38,11 +27,9 @@ class ExistingProfileFixtureManager(FixtureManager):
 
         from aiida import load_dbenv
         load_dbenv(profile=test_profile)
-        from aiida.settings import settings
-        settings.AIIDADB_PROFILE = test_profile
+        check_if_tests_can_run()
 
         # Running on test profile
-        self.profile_info["backend"] = "django"
         self._is_running_on_test_profile = True
 
         from aiida.backends.djsite.db.testbase import DjangoTests
@@ -60,7 +47,6 @@ class ExistingProfileFixtureManager(FixtureManager):
 _GLOBAL_FIXTURE_MANAGER = ExistingProfileFixtureManager()
 @contextmanager
 def fixture_manager():
-
     try:
         if not _GLOBAL_FIXTURE_MANAGER.has_profile_open():
             _GLOBAL_FIXTURE_MANAGER.backend = 'django'
@@ -70,4 +56,3 @@ def fixture_manager():
         yield _GLOBAL_FIXTURE_MANAGER
     finally:
         _GLOBAL_FIXTURE_MANAGER.reset_db()
-
