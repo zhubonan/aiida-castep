55e484968f7dc2b120aabccbb916d9b49e339ed7
Bonan Zhu <zhubonan@outlook.comBonan Zhu>
2019-03-28 09:01:22 +0000

    Bug fix, registration -> returning results is working

--- aiida_castep/tests/data/mock_castep.py
+++ aiida_castep/tests/data/mock_castep.py
@@ -11,6 +11,8 @@ import sys
 import json
 from pathlib import Path
 
+import click
+
 
 def get_hash(dict_obj):
     """
@@ -51,53 +53,67 @@ class MockOutput(object):
         Initialize the object
         """
         if base_dir is None:
-            self.base_dir = Path(__file__).parent
+            self.base_dir = Path(__file__).parent.resolve()
         else:
-            self.base_dir = Path(base_dir)
+            self.base_dir = Path(base_dir).resolve()
 
-    def calc_hash(self, path):
+    def calculate_hash(self, path):
 
+        path = str(path)
         param = castepinput.ParamInput.from_file(path + '.param', plain=True)
         cell = castepinput.CellInput.from_file(path + '.cell', plain=True)
         all_inp = dict(param)
         all_inp.update(cell)
         all_inp.pop('comment', None)
         all_inp.pop('COMMENT', None)
+        return get_hash(all_inp)[0]
 
     @property
     def _reg_file(self):
+        """
+        Path to the registry file
+        """
         return self.base_dir / 'registry.json'
 
     @property
     def registry(self):
-
+        """
+        Registry, a dictionary loaded from the json
+        """
         if not self._reg_file.is_file():
             return {}
 
-        with open(str(self._reg_file), 'w') as fh:
+        with open(str(self._reg_file), 'r') as fh:
             reg = json.load(fh)
         return reg
 
-    def register(self, path):
+    def register(self, seedpath):
         """
-        Register the hash and path
+        Register completed calculation. Such calculation must be in the directroy
+        tree of the mock_castep.py
         """
-        hash_ = self.calc_hash(path)
-        if not self._reg_file.is_file():
-            return {}
+        seedpath = Path(seedpath).resolve()
+        hash_ = self.calculate_hash(seedpath)
 
-        with open(str(self._reg_file)) as fh:
-            reg = json.load(fh)
+        if self._reg_file.is_file():
+            try:
+                with open(str(self._reg_file)) as fh:
+                    reg = json.load(fh)
+            except RuntimeError:
+                reg = {}
+        else:
+            reg = {}
 
-        rel_path = path.relative_to(self.base_dir)
+        # Relative seedpath to the folder
+        rel_path = seedpath.relative_to(self.base_dir).parent
         reg[hash_] = str(rel_path)
 
         with open(str(self._reg_file), 'w') as fh:
-            json.dump(fh, reg)
+            json.dump(reg, fh)
 
     def copy_results(self, rel_path):
         """
-        Copy the results
+        Copy existing calculation to the folder
         """
         print('Selected path:' , rel_path)
         import shutil
@@ -110,7 +126,10 @@ class MockOutput(object):
 
     def run(self, seedname):
         """
-        Run the 'Calculation'
+        Run the 'Calculation', if the seed is known we just copy
+        the results
+        Can be overiden by the 'MOCK_CALC' environmental varaible
+        , which define the folder of results to be copied
         """
         import os
         overide = os.environ.get('MOCK_CALC')
@@ -120,19 +139,27 @@ class MockOutput(object):
             print('Returning results from {}'.format(Path(overide).resolve()))
             return
 
-        hash_ = self.calc_hash(seedname)
+        hash_ = self.calculate_hash(seedname)
         reg = self.registry
 
-        res_folder = reg.get(hash_, None)
-        if res_folder:
-            self.copy_results(res_folder)
-            print('Returning results from {}'.format(Path(res_folder).resolve()))
+        known_result = reg.get(hash_, None)
+        if known_result:
+            self.copy_results(known_result)
+            print('Returning results from {}'.format(self.base_dir / known_result))
         else:
             raise RuntimeError('Results not registered')
 
+@click.command('mock')
+@click.option('--reg', default=False, is_flag=True,
+              help='Register the calculation')
+@click.argument('seed')
+def main(seed, reg):
+    runner = MockOutput()
+    if reg:
+        runner.register(seed)
+    else:
+        runner.run(seed)
 
 if __name__ == "__main__":
+    main()
 
-    seedname = sys.argv[1]
-    runner = MockOutput()
-    runner.run(seedname)
