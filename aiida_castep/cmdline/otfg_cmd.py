b6f59abce8b6f2e4c75cb3d309f424da824c06e3
Bonan Zhu <zhubonan@outlook.com>
2020-10-24 21:34:23 +0100

    Refector the pseudos related commands
    Unified with a single interface castep-pseudos

--- aiida_castep/cmdline/otfg_cmd.py
+++ aiida_castep/cmdline/otfg_cmd.py
@@ -7,18 +7,17 @@ import click
 
 from aiida.cmdline.commands.cmd_data import verdi_data
 
+# pylint: disable=import-outside-toplevel
 
-@verdi_data.group('castep-otfg')
-def otfg_cmd():
+
+@verdi_data.group('castep-pseudos')
+def pseudos_cmd():
     """Commandline interface for working with OTFGData"""
-    pass
-
-
-@otfg_cmd.command(name="migrate-families")
+@pseudos_cmd.command(name="migrate-families")
 def migrate_families():
     """
     Migrate to new style Group for pseudopotential families as implemented in
-    `aiida-core >= 1.2.0`. 
+    `aiida-core >= 1.2.0`.
 
     This command should be run once after upgrading `aiida-castep` to 1.2.0,
     to re-enable working with pseudopotential families.
@@ -30,7 +29,7 @@ def migrate_families():
         "Finished! Note: No need to run this command again for this profile.")
 
 
-@otfg_cmd.command(name="listfamilies")
+@pseudos_cmd.command(name="listfamilies")
 @click.option(
     '--element',
     '-e',
@@ -39,15 +38,14 @@ def migrate_families():
     "Show families contenting this element only. Can be passed multiple times")
 @click.option('--with_description', '-d', is_flag=True)
 def listfamilies(element, with_description):
-    """List avaliable OtfgData families"""
+    """List available OtfgData families"""
     from aiida.orm import QueryBuilder, Node
     from aiida_castep.data.otfg import OTFGGroup
-    from aiida.plugins import DataFactory
 
-    q = QueryBuilder()
-    q.append(Node, tag="otfgdata")
+    qbd = QueryBuilder()
+    qbd.append(Node, tag="otfgdata")
     if element:
-        q.add_filter("otfgdata", {
+        qbd.add_filter("otfgdata", {
             "attributes.element": {
                 "or": [{
                     'in': element
@@ -56,30 +54,30 @@ def listfamilies(element, with_description):
                 }]
             }
         })
-    q.append(OTFGGroup,
-             tag='group',
-             with_node='otfgdata',
-             project=['label', 'description'])
-    q.distinct()
-    if q.count() > 0:
-        for res in q.dict():
+    qbd.append(OTFGGroup,
+               tag='group',
+               with_node='otfgdata',
+               project=['label', 'description'])
+    qbd.distinct()
+    if qbd.count() > 0:
+        for res in qbd.dict():
             group_label = res.get("group").get("label")
             group_desc = res.get("group").get("description")
             # Count the number of pseudos in this group
-            q = QueryBuilder()
-            q.append(OTFGGroup,
-                     tag='thisgroup',
-                     filters={"label": {
-                         'like': group_label
-                     }})
-            q.append(Node, project=["id"], with_group='thisgroup')
+            qbd = QueryBuilder()
+            qbd.append(OTFGGroup,
+                       tag='thisgroup',
+                       filters={"label": {
+                           'like': group_label
+                       }})
+            qbd.append(Node, project=["id"], with_group='thisgroup')
 
             if with_description:
                 description_string = ": {}".format(group_desc)
             else:
                 description_string = ""
 
-            click.echo("* {} [{} pseudos]{}".format(group_label, q.count(),
+            click.echo("* {} [{} pseudos]{}".format(group_label, qbd.count(),
                                                     description_string))
 
     else:
