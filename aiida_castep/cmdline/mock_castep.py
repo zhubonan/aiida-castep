46183ca5f6e8aa0278a6e7dd613e2b92943cf8f9
Bonan Zhu <zhubonan@outlook.com>
2022-05-26 22:10:20 +0100

    Fix a critial bug involving setting up input structures
    Switching to castepinput for generating input files requires some
    code changes that was not previously carried out.
    
    Update example notebook for AiiDA v2.

--- aiida_castep/cmdline/mock_castep.py
+++ aiida_castep/cmdline/mock_castep.py
@@ -7,8 +7,6 @@ import os
 from pathlib import Path
 import click
 
-from aiida.cmdline.utils.decorators import with_dbenv
-
 from aiida_castep.utils.mock_code import MockRegistry, MockCastep
 
 
@@ -23,30 +21,51 @@ def output_file(*args):
 
 @click.command('mock-castep')
 @click.argument('seed')
-@with_dbenv()
 def mock_castep(seed):
     """
     A 'mock' CASTEP code that throws out output files for a given input seed name.
     """
-    from aiida.manage.configuration.settings import AIIDA_CONFIG_FOLDER  # pylint: disable=import-outside-toplevel
-    pwd = Path(os.getcwd()).absolute()
+    pwd = Path().absolute()
+    mock_output = []
 
-    aiida_path = Path(AIIDA_CONFIG_FOLDER)
-    aiida_cfg = aiida_path / 'config.json'
-    click.echo('DEBUG: AIIDA_PATH = {}'.format(os.environ.get('AIIDA_PATH')))
-    click.echo('DEBUG: AIIDA_CONFIG_FOLDER = {}'.format(str(aiida_path)))
-    assert aiida_path.exists()
-    assert aiida_cfg.is_file()
-    click.echo(aiida_cfg.read_text())
-    param = pwd / f'{seed}.param'
-    assert param.is_file(), '<seed>.param input file was not found.'
+    mock_output.append('MOCK PREPEND: START ----------------------\n')
+    mock_output.append('MOCK PREPEND: Mock directory: ' + str(pwd) + '\n')
+    mock_output.append('MOCK PREPEND: AIIDA_PATH: ' +
+                       os.environ.get('AIIDA_PATH', '') + '\n')
 
-    cell = pwd / f'{seed}.cell'
-    assert cell.is_file(), '<seed>.cell input file was not found.'
+    if not Path(f"{seed}.cell").is_file:
+        mock_output.append(f'MOCK PREPEND: {seed}.cell is not found.\n')
+        stop_and_return(mock_output)
+
+    if not Path(f"{seed}.param").is_file:
+        mock_output.append(f'MOCK PREPEND: {seed}.param is not found.\n')
+        stop_and_return(mock_output)
 
     mock_registry_path = os.environ.get('MOCK_CODE_BASE',
                                         data_path('registry'))
+
     click.echo('DEBUG: MOCK REGSISTRY PATH = {}'.format(
         str(mock_registry_path)))
-    mock = MockCastep(pwd, MockRegistry(mock_registry_path))
-    mock.run()
+    registry = MockRegistry(mock_registry_path)
+    mock = MockCastep(pwd, registry, seed)
+    if mock.is_runnable:
+        detected_path = mock.registry.get_path_by_hash(
+            registry.compute_hash(pwd))
+        mock_output.append(
+            f'MOCK PREPEND: Using test data in path {detected_path} based detection from inputs.\n'
+        )
+        mock.run()
+    else:
+        mock_output.append('MOCK PREPEND: No match was found.\n')
+        stop_and_return(mock_output)
+
+    # Write the stored information to the stdout file
+    with open('_scheduler-stdout.txt', 'a', encoding='utf8') as handler:
+        handler.write(''.join(mock_output))
+
+
+def stop_and_return(castep_mock_output):
+    """Halts castep.mock, rebuilds the castep_output and returns."""
+    # Assemble the
+    print(''.join(castep_mock_output))
+    raise RuntimeError('The castep.mock code could not perform a clean run.')
