7b0d7a40098c037622d05e4a9635691269bf3b26
Alp Eren Samli <74989695+asamli@users.noreply.github.com>
2022-06-10 15:27:37 +0100

    Desorting charges and spins from Mulliken parser
    Desorted the charges and spins from the Mulliken parser in raw_parser.py

--- aiida_castep/parsers/castep.py
+++ aiida_castep/parsers/castep.py
@@ -219,6 +219,16 @@ class CastepParser(Parser):
                 bands_node = bands_to_bandsdata(**bands_data)
             self.out(out_ln['bands'], bands_node)
 
+        ######## --- PROCESSING MULLIKEN DATA --- ########
+        input_structure = self.node.inputs.structure
+        idesort = get_desort_args(input_structure)
+        if len(out_dict["charges"]) > 1:
+            new_charges = np.array(out_dict["charges"])[idesort]
+            out_dict["charges"] = new_charges
+        if len(out_dict["spins"]) > 1:
+            new_spins = np.array(out_dict["spins"])[idesort]
+            out_dict["spins"] = new_spins
+
         ######## --- PROCESSING STRUCTURE DATA --- ########
         no_optimise = False
         try:
